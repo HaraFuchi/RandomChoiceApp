9e49db52c12b21fd3c6dd876ff446b7a376e3075
Ayano Hara <ayano0127@gmail.com>
2020-07-19 16:32:37 +0900

    Initial Commit

---
+++ RandomChoiceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomChoiceApp
+//
+//  Created by AYANO HARA on 2020/07/19.
+//  Copyright © 2020 AYANO HARA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
