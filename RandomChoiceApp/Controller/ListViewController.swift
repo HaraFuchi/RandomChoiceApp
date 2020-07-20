7bf00b0933b9809161dcd8298c05177535ec939f
Ayano Hara <ayano0127@gmail.com>
2020-07-20 16:43:23 +0900

    ListVCのUI作成とListPageTableViewCell.xibのUIを見やすく修正

--- RandomChoiceApp/Controller/ListViewController.swift
+++ RandomChoiceApp/Controller/ListViewController.swift
@@ -8,23 +8,49 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
-
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        let listPageNib = UINib(nibName: "ListPageTableViewCell", bundle: nil)
+        tableView.register(listPageNib, forCellReuseIdentifier: "ListPageCell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "お店一覧"
+    }
+    
+    //UI作成のため、一旦セルの数は１に設定しています
+    //本来は、新規登録画面で保存された内容・数のセルを表示
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListPageCell", for: indexPath)
+        
+        
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //後ほど、追加する機能 ↓
+        //セルをタップしたら詳細ページに遷移させる
+    }
 }
