d8d98c178a9282af94e8e8706de601fe94526f73
Ayano Hara <ayano0127@gmail.com>
2020-07-20 21:11:07 +0900

    変数名を変更, 空欄を削除, セクションのヘッダーを削除

--- RandomChoiceApp/Controller/ListViewController.swift
+++ RandomChoiceApp/Controller/ListViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    
-    @IBOutlet weak var goToSignupVCBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var signupVCBarButtonItem: UIBarButtonItem!
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var searchButton: UIButton!
-    @IBOutlet weak var sortingItemBottun: UIButton!
+
+    @IBOutlet weak var sortItemBottun: UIButton!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -29,10 +29,6 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         return 1
     }
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "お店一覧"
-    }
-    
     //UI作成のため、一旦セルの数は１に設定しています
     //本来は、新規登録画面で保存された内容・数のセルを表示
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -41,10 +37,6 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListPageCell", for: indexPath)
-        
-        
-        
-        
         return cell
     }
     
@@ -54,7 +46,6 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
         //後ほど、追加する機能 ↓
         //セルをタップしたら詳細ページに遷移させる
     }
