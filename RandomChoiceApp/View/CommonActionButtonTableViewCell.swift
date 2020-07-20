a9e6600c916fdc8bb5a29536704eb15ae824a585
fuchi0741 <fuchi.kazuma@gmail.com>
2020-07-21 01:46:13 +0900

    新規登録画面ボタンのデザイン

--- RandomChoiceApp/View/CommonActionButtonTableViewCell.swift
+++ RandomChoiceApp/View/CommonActionButtonTableViewCell.swift
@@ -9,13 +9,33 @@
 import UIKit
 
 class CommonActionButtonTableViewCell: UITableViewCell {
-
+    
+    //outlet
+    @IBOutlet weak var singupButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    //action
+    @IBAction func touchedSignupButton(_ sender: UIButton) {
+    }
+    @IBAction func touchedCancelButton(_ sender: UIButton) {
+    }
+   
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupButtons()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
     
+    // MARK: - Private
+    private func setupButtons(){
+        singupButton.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        singupButton.layer.borderWidth = 1.0
+        singupButton.layer.cornerRadius = 28.0
+        cancelButton.layer.borderColor = #colorLiteral(red: 0.9972122312, green: 0.4152126908, blue: 0.3679206967, alpha: 1)
+        cancelButton.layer.borderWidth = 1.0
+        cancelButton.layer.cornerRadius = 28.0
+    }
 }
