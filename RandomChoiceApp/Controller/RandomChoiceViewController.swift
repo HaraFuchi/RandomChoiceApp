ab3e16092653a5f47aed4be7c9a6fea17ad85fed
Ayano Hara <ayano0127@gmail.com>
2020-07-21 22:07:43 +0900

    enumを消去

--- RandomChoiceApp/Controller/RandomChoiceViewController.swift
+++ RandomChoiceApp/Controller/RandomChoiceViewController.swift
@@ -12,27 +12,27 @@ class RandomChoiceViewController: UIViewController, UITableViewDelegate, UITable
     
     @IBOutlet weak var tableView: UITableView!
     
-    enum Cell: Int, CaseIterable {
-        case firstCustomViewCell
-        case secondCustomViewCell
-        case thirdCustomViewCell
-
-        var cellIdentifier: String {
-            switch self {
-            case .firstCustomViewCell: return "ListPageTableViewCell"
-            case .secondCustomViewCell: return "SelectConditionsTableViewCell"
-            case .thirdCustomViewCell: return "RandomChoiceButtonTableViewCell"
-            }
-        }
-    }
+    //    enum Cell: Int, CaseIterable {
+    //        case firstCustomViewCell
+    //        case secondCustomViewCell
+    //        case thirdCustomViewCell
+    //
+    //        var cellIdentifier: String {
+    //            switch self {
+    //            case .firstCustomViewCell: return "ListPageTableViewCell"
+    //            case .secondCustomViewCell: return "SelectConditionsTableViewCell"
+    //            case .thirdCustomViewCell: return "RandomChoiceButtonTableViewCell"
+    //            }
+    //        }
+    //    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(UINib(nibName: "ListPageTableViewCell", bundle: nil), forCellReuseIdentifier: "ListPageTableViewCell")
-        tableView.register(UINib(nibName: "SelectConditionsTableViewCell", bundle: nil), forCellReuseIdentifier: "SelectConditionsTableViewCell")
-        tableView.register(UINib(nibName: "RandomChoiceButtonTableViewCell", bundle: nil), forCellReuseIdentifier: "RandomChoiceButtonTableViewCell")
+        tableView.register(UINib(nibName: "ListPageTableViewCell", bundle: nil), forCellReuseIdentifier: "ListPagewCell")
+        tableView.register(UINib(nibName: "SelectConditionsTableViewCell", bundle: nil), forCellReuseIdentifier: "SelectConditionsCell")
+        tableView.register(UINib(nibName: "RandomChoiceButtonTableViewCell", bundle: nil), forCellReuseIdentifier: "RandomChoiceButtonCell")
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -40,26 +40,29 @@ class RandomChoiceViewController: UIViewController, UITableViewDelegate, UITable
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Cell.allCases.count
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellType = Cell(rawValue: indexPath.row)!
-        switch cellType {
-        case .firstCustomViewCell:
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellType.cellIdentifier) as! ListPageTableViewCell
+        
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ListPagewCell") as! ListPageTableViewCell
             return cell
-        case .secondCustomViewCell:
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellType.cellIdentifier) as! SelectConditionsTableViewCell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SelectConditionsCell") as! SelectConditionsTableViewCell
             return cell
-        case .thirdCustomViewCell:
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellType.cellIdentifier) as! RandomChoiceButtonTableViewCell
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RandomChoiceButtonCell") as! RandomChoiceButtonTableViewCell
             return cell
+        default:
+            break
         }
+        return UITableViewCell()
     }
     
-   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-       return 250
-   }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
+    }
 }
 
