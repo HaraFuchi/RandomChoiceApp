4ef08b6f4aba7a595479ed25b34972eb65ccc2e6
Ayano Hara <ayano0127@gmail.com>
2020-07-21 15:28:14 +0900

    RandomChoiceVCの<場所><ジャンル>選択のUIを作成

--- RandomChoiceApp/Controller/RandomChoiceViewController.swift
+++ RandomChoiceApp/Controller/RandomChoiceViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class RandomChoiceViewController: UIViewController {
-
+class RandomChoiceViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+//        let resultRestaurantNib = UINib(nibName: "ListPageTableViewCell", bundle: nil)
+//        tableView.register(resultRestaurantNib, forCellReuseIdentifier: "ListPageCell")
+        let selectConditionsNib = UINib(nibName: "SelectConditionsTableViewCell", bundle: nil)
+        tableView.register(selectConditionsNib, forCellReuseIdentifier: "ConditonsCell")
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let selectConditionsCell = tableView.dequeueReusableCell(withIdentifier: "ConditonsCell", for: indexPath)
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
+    }
 }
 
