c4d2a656393f38fcb8f19ab3c3b90d51bd972f16
fuchi0741 <fuchi.kazuma@gmail.com>
2020-08-17 08:50:39 +0900

    匿名ログイン実装をModel化のためのファイル作成

---
+++ RandomChoiceApp/Model/AnonymousLoginModel.swift
@@ -0,0 +1,13 @@
+//
+//  AnonymousLoginModel.swift
+//  RandomChoiceApp
+//
+//  Created by 渕一真 on 2020/08/17.
+//  Copyright © 2020 AYANO HARA. All rights reserved.
+//
+
+import Foundation
+
+class AnonymousLoginModel {
+    
+}
