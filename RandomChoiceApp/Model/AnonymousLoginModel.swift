b804c9b5af2bdc9f0d44b1caa1ca270339251bf2
fuchi0741 <fuchi.kazuma@gmail.com>
2020-08-17 09:04:08 +0900

    匿名ログインのロジックをModelに記述

--- RandomChoiceApp/Model/AnonymousLoginModel.swift
+++ RandomChoiceApp/Model/AnonymousLoginModel.swift
@@ -7,7 +7,22 @@
 //
 
 import Foundation
+import Firebase
 
+//匿名ログイン認証
 class AnonymousLoginModel {
     
+    func anonymousLogin() {
+        // 匿名認証(下記のメソッドがエラーなく終了すれば、認証完了する)
+        Auth.auth().signInAnonymously() { (authResult, error) in
+            if let error = error {
+                print("Auth Error :\(error.localizedDescription)")
+            }
+            // 認証情報の取得
+            guard let user = authResult?.user else { return }
+            let isAnonymous = user.isAnonymous  // true
+            let uid = user.uid
+            return
+        }
+    }
 }
