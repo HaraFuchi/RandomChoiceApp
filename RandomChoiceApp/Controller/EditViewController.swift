98b0d0a2eb4c20f1e5dec739b146754269c9e041
Ayano Hara <ayano0127@gmail.com>
2020-08-28 19:39:26 +0900

    EditViewControllerを追加

---
+++ RandomChoiceApp/Controller/EditViewController.swift
@@ -0,0 +1,70 @@
+//
+//  EditViewController.swift
+//  RandomChoiceApp
+//
+//  Created by AYANO HARA on 2020/08/28.
+//  Copyright © 2020 AYANO HARA. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    enum CategoryList: String, CaseIterable{
+        case storeName = "店名"
+        case placeName = "場所"
+        case genreName = "ジャンル"
+        
+        var CategoryPlaceHolderList: String {
+            switch self {
+            case .storeName: return "例)サイゼリア"
+            case .placeName: return "例)新宿"
+            case .genreName: return "例)イタリアン"
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        let sinupCategoryNib = UINib(nibName: "SignupCategoryTableViewCell", bundle: nil)
+        let signupAndCancelButtonCell = UINib(nibName: "CommonActionButtonTableViewCell", bundle: nil)
+        tableView.register(sinupCategoryNib, forCellReuseIdentifier: "SignupCell")
+        tableView.register(signupAndCancelButtonCell, forCellReuseIdentifier: "ActionButtonCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CategoryList.allCases.count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let categoryCell = tableView.dequeueReusableCell(withIdentifier: "SignupCell", for: indexPath) as! SignupCategoryTableViewCell
+        let signupAndCancelButtonCell = tableView.dequeueReusableCell(withIdentifier: "ActionButtonCell", for: indexPath) as! CommonActionButtonTableViewCell
+        
+        switch indexPath.row {
+        case 0:
+            categoryCell.categoryLabel.text = CategoryList.storeName.rawValue
+            categoryCell.categoryTextField.placeholder = CategoryList.storeName.CategoryPlaceHolderList
+            categoryCell.IndexPathNumber = indexPath.row
+            return categoryCell
+        case 1:
+            categoryCell.categoryLabel.text = CategoryList.placeName.rawValue
+            categoryCell.categoryTextField.placeholder = CategoryList.placeName.CategoryPlaceHolderList
+            categoryCell.IndexPathNumber = indexPath.row
+            return categoryCell
+        case 2:
+            categoryCell.categoryLabel.text = CategoryList.genreName.rawValue
+            categoryCell.categoryTextField.placeholder = CategoryList.genreName.CategoryPlaceHolderList
+            categoryCell.IndexPathNumber = indexPath.row
+            return categoryCell
+        case 3:
+            return signupAndCancelButtonCell
+        default:
+            break
+        }
+        return UITableViewCell()
+    }
+}
