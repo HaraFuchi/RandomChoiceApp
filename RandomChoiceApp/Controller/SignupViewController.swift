2215899f54f14173a0c515b7101bad4815a9ef50
fuchi0741 <fuchi.kazuma@gmail.com>
2020-08-30 06:46:35 +0900

    TFにの場合にnilを返す

--- RandomChoiceApp/Controller/SignupViewController.swift
+++ RandomChoiceApp/Controller/SignupViewController.swift
@@ -106,6 +106,7 @@ extension SignupViewController {
     private func showSignupAlert() {
         let alert = UIAlertController(title: "お店を登録しますか？", message: nil, preferredStyle: .alert)
         let signupAction = UIAlertAction(title: "登録する", style: .default) { _ in
+            self.textConvertNil()
             if self.storeNameString == nil, self.placeNameString == nil, self.genreNameString == nil {
                 self.showAllNoTextField()
             } else {
@@ -126,5 +127,19 @@ extension SignupViewController {
         let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
         alert.addAction(cancelAction)
         present(alert, animated: true, completion: nil)
+    }
+    
+    //TFが""の場合Cellのレイアウトが崩れるため、nilを返す
+    //nilの場合は「???」が返されレイアウトは崩れない
+    private func textConvertNil() {
+        if storeNameString == "" {
+            storeNameString = nil
+        }
+        if placeNameString == "" {
+            placeNameString = nil
+        }
+        if genreNameString == "" {
+            genreNameString = nil
+        }
     }
 }
