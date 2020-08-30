34624d0d748d0ede2cc8f9a21493eb025ad1b103
fuchi0741 <fuchi.kazuma@gmail.com>
2020-08-30 19:54:21 +0900

    リファクタリングによるbuildエラー

--- RandomChoiceApp/Controller/ListViewController.swift
+++ RandomChoiceApp/Controller/ListViewController.swift
@@ -56,10 +56,10 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
 
 //MARK: - Private func
 extension ListViewController {
-    private func showDeleteAlert(tableView: UITableView, editingStyle: UITableViewCell.EditingStyle, indexpath: IndexPath) {
+    private func showDeleteAlert(tableView: UITableView, editingStyle: UITableViewCell.EditingStyle, indexPath: IndexPath) {
         let showAlert = UIAlertController(title: AlertTitleLiteral.delete, message: nil, preferredStyle: .alert)
         let deleteAction = UIAlertAction(title: AlertButtonLiteral.delete, style: .destructive, handler: { _ -> Void in
-            self.crudModel.deleteStoreInfo(indexpath: indexpath)
+            self.crudModel.deleteStoreInfo(indexPath: indexPath)
             if editingStyle == UITableViewCell.EditingStyle.delete {
                 self.crudModel.storeDataArray.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
--- RandomChoiceApp/Controller/SignupViewController.swift
+++ RandomChoiceApp/Controller/SignupViewController.swift
@@ -122,7 +122,7 @@ extension SignupViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    private func showAllNoTextField() {
+    private func showAlertAllNilTextField() {
         let alert = UIAlertController(title: AlertTitleLiteral.allTextEmpty, message: AlertMessageLiteral
             .allTextEmpty, preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: AlertButtonLiteral.OK, style: .cancel, handler: nil)
