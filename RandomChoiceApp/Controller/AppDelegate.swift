465e3b6916719d0a945f661b12d9543c6e84260d
fuchi0741 <fuchi.kazuma@gmail.com>
2020-09-24 10:08:15 +0900

    Build Schemeを追加

--- RandomChoiceApp/Controller/AppDelegate.swift
+++ RandomChoiceApp/Controller/AppDelegate.swift
@@ -19,6 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         anonymousLoginModel.anonymousLogin()
+        
+        #if DEV
+            print("DEV")
+        #else
+            print("OTHER")
+        #endif
         return true
     }
     
