16ed83286eab7bb9216d5aef82b7d8817c1249e4
Ayano Hara <ayano0127@gmail.com>
2020-11-23 23:27:42 +0900

    カスタムセルを作成

---
+++ RandomChoiceApp/View/Cells/SettingTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SettingTableViewCell.swift
+//  RandomChoiceApp
+//
+//  Created by AYANO HARA on 2020/11/23.
+//  Copyright © 2020 AYANO HARA. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
