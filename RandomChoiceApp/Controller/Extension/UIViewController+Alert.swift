22229efc5ede3ee453a1171e82c06a215ae3c2cb
Ayano Hara <ayano0127@gmail.com>
2020-12-11 19:10:42 +0900

    オフライン時にのみ呼ばれるアラートを作成

--- RandomChoiceApp/Controller/Extension/UIViewController+Alert.swift
+++ RandomChoiceApp/Controller/Extension/UIViewController+Alert.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import Reachability
 
 protocol AlertDisplayable { }
 
 extension AlertDisplayable where Self: UIViewController {
     func showAlertAllNilTextField() {
         let alert = UIAlertController(title: AlertTitleLiteral.allTextEmpty, message: AlertMessageLiteral
-            .allTextEmpty, preferredStyle: .alert)
+                                        .allTextEmpty, preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: AlertButtonLiteral.OK, style: .cancel, handler: nil)
         alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //オフライン時にのみ呼ばれるアラート
+    func showAlertOffline() {
+        let reachability = try! Reachability()
+        let alert = UIAlertController(title: "エラー", message: "インターネット接続状況を確認して再度お試しください。", preferredStyle: .alert)
+        let reloadAction = UIAlertAction(title: "再試行", style: .default, handler: { [self] _ in
+            if reachability.connection == .unavailable {
+                showAlertOffline()
+                print("接続なし")
+            }
+        })
+        alert.addAction(reloadAction)
         present(alert, animated: true, completion: nil)
     }
 }
