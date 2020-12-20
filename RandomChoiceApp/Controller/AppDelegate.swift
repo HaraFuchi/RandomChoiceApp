0adc1c1ad1d8e2ea49c8c47b339623b3788b8858
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2020-12-20 19:36:48 +0900

    冗長な命名をリネーム

--- RandomChoiceApp/Controller/AppDelegate.swift
+++ RandomChoiceApp/Controller/AppDelegate.swift
@@ -14,14 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    private let anonymousLoginModel = AnonymousLoginModel()
     private let launchTime: UInt32 = 1
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
         if Auth.auth().currentUser == nil {
-            anonymousLoginModel.anonymousLogin()
+            UserLoginModel.anonymous()
         }
         
         sleep(launchTime)
