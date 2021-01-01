314f1393f8c03e976affc7461f5ff04ea746ffaf
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-01-02 02:32:19 +0900

    ファイル追加

---
+++ RandomChoiceApp/Model/ResultStoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  ResultStoreDataModel.swift
+//  RandomChoiceApp
+//
+//  Created by 渕一真 on 2021/01/02.
+//  Copyright © 2021 AYANO HARA. All rights reserved.
+//
+
+import Foundation
+
+class ResultStoreDataModel {
+    
+}
