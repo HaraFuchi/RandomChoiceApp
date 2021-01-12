5e1886506c191dba726cb1f12dbd2a904f4c8333
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-01-13 03:43:57 +0900

    編集と登録の色の判別ロジックの可読性が低かったため修正

--- RandomChoiceApp/View/Cells/CommonActionButtonTableViewCell.swift
+++ RandomChoiceApp/View/Cells/CommonActionButtonTableViewCell.swift
@@ -44,29 +44,47 @@ extension CommonActionButtonTableViewCell {
     private func setupDetailCell() {
         self.selectionStyle = .none
     }
-    //FIXME 同じようなプロパティを繰り返し使用しているため修正する
+    
     func setupButton(_ VC: UIViewController) {
-        setUpCommonButton()
         if VC is SignupViewController {
-            signUpButton.setTitle(ButtonTitle.signUp, for: .normal)
-            signUpButton.backgroundColor = #colorLiteral(red: 0.9972122312, green: 0.4152126908, blue: 0.3679206967, alpha: 1)
-            cancelButton.setTitle(ButtonTitle.cancel, for: .normal)
-            cancelButton.setTitleColor(#colorLiteral(red: 0.9972122312, green: 0.4152126908, blue: 0.3679206967, alpha: 1), for: .normal)
-            cancelButton.layer.borderColor = #colorLiteral(red: 0.9972122312, green: 0.4152126908, blue: 0.3679206967, alpha: 1)
+            signupButtonDetailInSignupScreen()
+            cancelButtonDetailInSignupScreen()
         } else if VC is EditViewController {
-            signUpButton.setTitle(ButtonTitle.saveEdit, for: .normal)
-            signUpButton.backgroundColor = #colorLiteral(red: 0.9921568627, green: 0.6393200201, blue: 0.218539124, alpha: 1)
-            cancelButton.setTitle(ButtonTitle.cancel, for: .normal)
-            cancelButton.setTitleColor(#colorLiteral(red: 0.9921568627, green: 0.6393200201, blue: 0.218539124, alpha: 1), for: .normal)
-            cancelButton.layer.borderColor = #colorLiteral(red: 0.9921568627, green: 0.6393200201, blue: 0.218539124, alpha: 1)
+            signupButtonDetailInEditScreen()
+            cancelButtonDetailInEditScreen()
         }
     }
     
-    private func setUpCommonButton() {
+    private func signupButtonDetailInSignupScreen() {
+        signUpButton.setTitle(ButtonTitle.signUp, for: .normal)
+        signUpButton.backgroundColor = #colorLiteral(red: 0.9972122312, green: 0.4152126908, blue: 0.3679206967, alpha: 1)
         signUpButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
         signUpButton.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         signUpButton.layer.borderWidth = 1.0
         signUpButton.layer.cornerRadius = 28.0
+    }
+    
+    private func cancelButtonDetailInSignupScreen() {
+        cancelButton.setTitle(ButtonTitle.cancel, for: .normal)
+        cancelButton.layer.borderColor = #colorLiteral(red: 0.9972122312, green: 0.4152126908, blue: 0.3679206967, alpha: 1)
+        cancelButton.setTitleColor(#colorLiteral(red: 0.9972122312, green: 0.4152126908, blue: 0.3679206967, alpha: 1), for: .normal)
+        cancelButton.layer.borderWidth = 1.0
+        cancelButton.layer.cornerRadius = 28.0
+    }
+    
+    private func signupButtonDetailInEditScreen() {
+        signUpButton.setTitle(ButtonTitle.saveEdit, for: .normal)
+        signUpButton.backgroundColor = #colorLiteral(red: 0.9921568627, green: 0.6393200201, blue: 0.218539124, alpha: 1)
+        signUpButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        signUpButton.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        signUpButton.layer.borderWidth = 1.0
+        signUpButton.layer.cornerRadius = 28.0
+    }
+    
+    private func cancelButtonDetailInEditScreen() {
+        cancelButton.setTitle(ButtonTitle.cancel, for: .normal)
+        cancelButton.layer.borderColor = #colorLiteral(red: 0.9921568627, green: 0.6393200201, blue: 0.218539124, alpha: 1)
+        cancelButton.setTitleColor(#colorLiteral(red: 0.9921568627, green: 0.6393200201, blue: 0.218539124, alpha: 1), for: .normal)
         cancelButton.layer.borderWidth = 1.0
         cancelButton.layer.cornerRadius = 28.0
     }
