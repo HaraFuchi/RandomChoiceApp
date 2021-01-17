1e0963d6ee744bfb0fe59bde2a13aadeb7236de3
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-01-17 16:05:04 +0900

    Void削除

--- RandomChoiceApp/View/Cells/RandomChoiceButtonTableViewCell.swift
+++ RandomChoiceApp/View/Cells/RandomChoiceButtonTableViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 protocol DiceButtonViewProtocal: AnyObject {
-    func didTapDiceButton() -> Void
+    func didTapDiceButton() 
 }
 
 class RandomChoiceButtonTableViewCell: UITableViewCell {
