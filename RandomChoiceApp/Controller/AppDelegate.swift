f41d7d2c155e72583bc4e3bdf83c73e0265063ec
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-01-20 02:28:01 +0900

    英語

--- RandomChoiceApp/Controller/AppDelegate.swift
+++ RandomChoiceApp/Controller/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
-        print("テストです")
+        print("✊TEST")
         
         if Auth.auth().currentUser == nil {
             UserLoginModel.anonymous()
