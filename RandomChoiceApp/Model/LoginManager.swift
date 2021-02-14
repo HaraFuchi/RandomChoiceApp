cd101f3f1a2e6046fd1b26f9c05272352630c493
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-02-14 16:09:22 +0900

    ログインファイルのリネーム

--- RandomChoiceApp/Model/LoginManager.swift
+++ RandomChoiceApp/Model/LoginManager.swift
@@ -1,15 +1,14 @@
 //
-//  UserLoginModel.swift
+//  LoginManager.swift
 //  RandomChoiceApp
 //
 //  Created by 渕一真 on 2020/08/17.
 //  Copyright © 2020 AYANO HARA. All rights reserved.
 //
 
-import Foundation
 import Firebase
 
-class UserLoginModel {
+struct LoginManager {
     static func anonymous() {
         Auth.auth().signInAnonymously { (authResult, error) in
             if let error = error {
