97735bb6a1ab255747944d415adb92762060b7ba
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-09-18 01:21:19 +0900

    リテラルを回避する変数を用意

---
+++ RandomChoiceApp/Controller/Extension/extension+NSObject.swift
@@ -0,0 +1,19 @@
+//
+//  extension+NSObject.swift
+//  RandomChoiceApp
+//
+//  Created by 渕一真 on 2021/09/18.
+//  Copyright © 2021 AYANO HARA. All rights reserved.
+//
+
+import Foundation
+
+extension NSObject {
+    class var className: String {
+        String(describing: self)
+    }
+    
+    var className: String {
+        type(of: self).className
+    }
+}
